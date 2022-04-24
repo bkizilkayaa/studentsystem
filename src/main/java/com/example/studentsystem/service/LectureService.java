aa70e1470756a3b27d8d49a43c33fcced5baccb4
Burak Kizilkaya <kizilkayaburakk@hotmail.com>
2022-04-25 00:12:35 +0300

    services added

---
+++ src/main/java/com/example/studentsystem/service/LectureService.java
@@ -0,0 +1,67 @@
+package com.example.studentsystem.service;
+
+import com.example.studentsystem.model.Lecture;
+import com.example.studentsystem.repository.LectureRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LectureService {
+    private LectureRepository lectureRepository;
+
+    public LectureService(LectureRepository lectureRepository) {
+        this.lectureRepository = lectureRepository;
+    }
+    public Lecture getLecture(int id) {
+        return lectureRepository.findById(id).orElse(null);
+    }
+    public Lecture addLecture(Lecture newLecture){
+        try{
+            return lectureRepository.save(newLecture);
+        }
+        catch(Exception ex){
+            return null;
+        }
+    }
+
+    public Boolean deleteLecture(int id){
+        try{
+            var lecture= getLecture(id);
+            lectureRepository.delete(lecture);
+            return true;
+        }
+        catch(Exception ex){
+            return null;
+        }
+    }
+
+    public Lecture updateLecture(Lecture newLecture, int id){
+        try{
+            var lectureToUpdated=getLecture(id);
+            lectureToUpdated.setLectureName(newLecture.getLectureName());
+            lectureToUpdated.setStudents(newLecture.getStudents());
+            return lectureRepository.save(lectureToUpdated);
+
+        }
+        catch(Exception ex){
+            return null;
+        }
+    }
+
+    public List<Lecture> getLectureList(){
+        var list=lectureRepository.findAll();
+        List<Lecture> lectureList= new ArrayList<>();
+        for(var ob:lectureList){
+            lectureList.add(ob);
+        }
+        if(lectureList.size()>0){
+            return lectureList;
+        }
+        else
+            return null;
+    }
+
+
+}
---
+++ src/main/java/com/example/studentsystem/service/StudentService.java
@@ -0,0 +1,69 @@
+package com.example.studentsystem.service;
+
+
+import com.example.studentsystem.model.Student;
+import com.example.studentsystem.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+    private StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+    public Student getStudent(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+    public Student addStudent(Student newStudent){
+        try{
+            return studentRepository.save(newStudent);
+        }
+        catch(Exception ex){
+            return null;
+        }
+    }
+
+    public Boolean deleteStudent(int id){
+        try{
+            var student= getStudent(id);
+            studentRepository.delete(student);
+            return true;
+        }
+        catch(Exception ex){
+            return null;
+        }
+    }
+    
+    public Student updateStudent(Student newStudent, int id){
+        try{
+            var studentToUpdated=getStudent(id);
+            studentToUpdated.setFullName(newStudent.getFullName());
+            studentToUpdated.setEmail(newStudent.getEmail());
+            studentToUpdated.setLectures(newStudent.getLectures());
+            return studentRepository.save(studentToUpdated);
+
+        }
+        catch(Exception ex){
+            return null;
+        }
+    }
+
+    public List<Student> getStudentList(){
+        var list=studentRepository.findAll();
+        List<Student> studentList= new ArrayList<>();
+        for(var ob:studentList){
+            studentList.add(ob);
+        }
+        if(studentList.size()>0){
+            return studentList;
+        }
+        else
+            return null;
+    }
+
+
+}
